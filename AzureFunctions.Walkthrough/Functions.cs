da7b2bb860ffd8c0126792f854939ca2f6c735f6
Deuri Vasquez <deuriib@gmail.com>
2023-11-05 18:12:46 -0400

    refactor:  rename Function1

---
+++ AzureFunctions.Walkthrough/Functions.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctions.Walkthrough;
+
+public static class Functions
+{
+    [Function("HttpFunction")]
+    public static async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "walkthrough")] 
+        HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("HttpFunction");
+        logger.LogInformation("C# HTTP trigger function processed a request");
+
+        var response = req.CreateResponse();
+        await response.WriteAsJsonAsync(new
+        {
+            Name = "Azure Function",
+            CurrentTime = DateTime.UtcNow
+        });
+
+        return response;
+        
+    }
+}
